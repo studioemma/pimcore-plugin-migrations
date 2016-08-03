1493093e9bc0089bb9dc39827e23c56ddb8012ed
Ike Devolder <ike.devolder@studioemma.eu>
2016-08-03 09:50:21 +0200

    check if the plugin is installed to offer options
    when not yet installed, only offer to install pimcore
    when installed, offer migrate and generate
    
    fixes #1
    
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- cli/console.php
+++ cli/console.php
@@ -6,12 +6,14 @@
 include_once(__DIR__ . "/../../../pimcore/cli/startup.php");
 
 use Symfony\Component\Console\Application;
+use Pimcore\ExtensionManager;
 
 $application = new Application('Pimcore Migrations', '1.0.0');
-try {
+// avoid relying on PHP7 only features when advertising PHP5 compatiblity
+if (ExtensionManager::isEnabled('plugin', 'Migrations')) {
     $application->add(new \Migrations\Console\Command\MigrateCommand());
     $application->add(new \Migrations\Console\Command\GenerateCommand());
-} catch (Error $err) {
+} else {
     include_once(
         realpath(
             __DIR__ . "/../lib/Migrations/Console/Command/InstallCommand.php"
